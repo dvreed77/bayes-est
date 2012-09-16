d7f1cab82e184c95af996b1b1c09721e085749f2
David Reed <david.reed.c@gmail.com>
2012-09-16 19:52:19 -0400

    Updated docs

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,20 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = [
+	'sphinx.ext.autodoc',
+	'sphinx.ext.doctest',
+	'sphinx.ext.intersphinx',
+	'sphinx.ext.todo',
+	'sphinx.ext.coverage',
+	'sphinx.ext.pngmath',
+	'matplotlib.sphinxext.mathmpl',
+    'matplotlib.sphinxext.only_directives',
+    'matplotlib.sphinxext.plot_directive',        
+    # 'ipython_directive',
+    'ipython_console_highlighting',
+    'inheritance_diagram',
+    'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
